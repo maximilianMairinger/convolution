4e0d4c7ad16de2f6e1a4e3be98bc2348b7e1cf52
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-08-23 21:34:03 +0200

    readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Convolution
 
-Simple direct and extendable convolution library.
+Perform convolution operations on one dimensional arrays. This library provides a flexible direct convolution implementation. The library supports both regular arrays and typed arrays.
 
-> Please note that Convolution is currently under development and not yet suited for production
+Note that this library does not provide a fast (Fast Fourier transform based) convolution implementation. If you are looking for a fast convolution implementation, take a look at [ndarray-convolve](http://npmjs.com/package/ndarray-convolve).
 
 ## Installation
 
@@ -12,13 +12,61 @@ Simple direct and extendable convolution library.
 
 ## Usage
 
+### Convolve
 
+`convolve(a: Array<T> | TypedArray, b: Array<T> | TypedArray): Array<T> | TypedArray`
+
+Performs a convolution operation on two arrays one dimensional `a` and `b`. The function returns a new array of the same type as the input, which represents the result of the convolution.
 
 ```ts
-import convolution from "convolution"
+import convolve from "convolution"
 
-convolution()
+const a = [1, 2, 3]
+const b = [1, 2, 3]
+
+const result = convolve(a, b)
+// result = [1, 4, 10, 12, 9]
 ```
+
+### Convolve Arbitrary
+
+`convolveArbitrary(convolutionStepFunction: (a: List, b: List) => T): (a: List, b: List) => List`
+
+Creates a convolution function given a custom convolution step function. A convolution step is the operation of combining two arrays of the same length into a single value. Typically this is done by `sum(a_i * b_i)`, but can be arbitrary. The function returns a new function which can be used to perform the whole convolution on two arrays.
+
+The following example is the same as the `convolve` function above.
+
+```ts
+import { convolveArbitrary } from "convolution"
+
+const convolutionStepFunction = (a, b) => {
+  let sum = 0
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i] * b[i]
+  }
+  return sum
+}
+
+const convolve = convolveArbitrary(convolutionStepFunction)
+
+const a = [1, 2, 3]
+const b = [1, 2, 3]
+
+const result = convolve(a, b)
+// result = [1, 4, 10, 12, 9]
+```
+
+## Supported Typed Arrays
+
+- Array
+- Int8Array
+- Uint8Array
+- Int16Array
+- Uint16Array
+- Int32Array
+- Uint32Array
+- Float32Array
+- Float64Array
 
 ## Contribute
 
--- app/src/convolution.ts
+++ app/src/convolution.ts
@@ -1,5 +1,55 @@
-export function convolution() {
-  
+type TypedArray = Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array
+
+
+export function convolveArbitrary<T, List extends T extends number ? (Array<T> | TypedArray) : (Array<T>)>(convolutionStepFunction: (a: List, b: List) => T) {
+  return function convolve(a: List, b: List) {
+    const List = (a as any).constructor as typeof Array<T> | ({ new(len: number): TypedArray } & { new(ar: Iterable<number>): TypedArray } & {prototype: TypedArray})
+    const aReversed = (a instanceof Array ? [...a] : new (a as any).constructor(a)).reverse()
+    const bReversed = (b instanceof Array ? [...b] : new (a as any).constructor(b)).reverse()
+    // const bReversed = b
+    const result = new List(a.length + (b as any).length - 1)
+
+      
+    for (let i = 0; i < (a as TypedArray).length; i++) {
+      const commonLength = Math.min(i + 1, (b as TypedArray).length)
+      const toBeConvA = new List(commonLength)
+      const toBeConvB = new List(commonLength)
+      
+      const missingLengthFromX = i + 1 - commonLength
+      for (let j = 0; j < commonLength; j++) {
+        toBeConvA[j] = (a as TypedArray)[missingLengthFromX + j]
+        toBeConvB[j] = (b as TypedArray)[j]
+      }
+      toBeConvB.reverse()
+      result[i] = convolutionStepFunction(toBeConvA as List, toBeConvB as List)
+    }
+    
+    for (let i = 0; i < (b as TypedArray).length - 1; i++) {
+      const commonLength = Math.min((a as TypedArray).length, (b as TypedArray).length - i - 1)
+      const toBeConvA = new List(commonLength)
+      const toBeConvB = new List(commonLength)
+      const missingLengthFromY = (b as TypedArray).length - i - 1 - commonLength
+      for (let j = 0; j < commonLength; j++) {
+        toBeConvA[j] = (aReversed as TypedArray)[j]
+        toBeConvB[j] = (bReversed as TypedArray)[missingLengthFromY + j]
+      }
+      toBeConvA.reverse()
+      result[(a as TypedArray).length + i] = convolutionStepFunction(toBeConvA as List, toBeConvB as List)
+    }
+    
+
+    return result
+  }
 }
 
-export default convolution
+
+export const convolve = convolveArbitrary((a: TypedArray | number[], b: TypedArray | number[]) => {
+  let sum = 0
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i] * b[i]
+  }
+  return sum
+})
+
+export default convolve
+
